4da7fb069f41c411156b0919f4b2b06c0397134a
Jumaana-bit <71558928+Jumaana-bit@users.noreply.github.com>
2024-03-24 03:22:35 -0400

    Update echo_server.c

--- echo_server.c
+++ echo_server.c
@@ -1,61 +1,81 @@
+/* A simple echo server using TCP */
 #include <stdio.h>
+#include <sys/types.h>
+#include <sys/unistd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <sys/signal.h>
+#include <sys/wait.h>
 #include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <arpa/inet.h>
-
-#define BUFFER_SIZE 1024
-
-int main(int argc, char *argv[]) {
-    if (argc != 2) {
-        fprintf(stderr, "Usage: %s <port_number>\n", argv[0]);
-        exit(EXIT_FAILURE);
-    }
-
-    int port = atoi(argv[1]);
-
-    int server_fd, new_socket;
-    struct sockaddr_in address;
-    int opt = 1;
-    int addrlen = sizeof(address);
-    char buffer[BUFFER_SIZE] = {0};
-    
-    // Creating socket file descriptor
-    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
-        perror("socket failed");
-        exit(EXIT_FAILURE);
-    }
-    
-    // Forcefully attaching socket to the port
-    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) {
-        perror("setsockopt");
-        exit(EXIT_FAILURE);
-    }
-    address.sin_family = AF_INET;
-    address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons(port);
-    
-    // Forcefully attaching socket to the port
-    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
-        perror("bind failed");
-        exit(EXIT_FAILURE);
-    }
-    if (listen(server_fd, 3) < 0) {
-        perror("listen");
-        exit(EXIT_FAILURE);
-    }
-    if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen)) < 0) {
-        perror("accept");
-        exit(EXIT_FAILURE);
-    }
-    
-    // Read message from client and echo back
-    int valread;
-    while ((valread = read(new_socket, buffer, BUFFER_SIZE)) > 0) {
-        printf("Received: %s\n", buffer);
-        send(new_socket, buffer, strlen(buffer), 0);
-        memset(buffer, 0, BUFFER_SIZE);
-    }
-    
-    return 0;
+#include <strings.h>
+#define SERVER_TCP_PORT 3000 /* well-known port */
+#define BUFLEN 256 /* buffer length */
+int echod(int);
+void reaper(int);
+int main(int argc, char **argv)
+{
+int sd, new_sd, client_len, port;
+struct sockaddr_in server, client;
+switch(argc){
+case 1:
+port = SERVER_TCP_PORT;
+break;
+case 2:
+port = atoi(argv[1]);
+break;
+default:
+fprintf(stderr, "Usage: %s [port]\n", argv[0]);
+exit(1);
+}
+/* Create a stream socket */
+if ((sd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
+fprintf(stderr, "Can't creat a socket\n");
+exit(1);
+}
+/* Bind an address to the socket */
+bzero((char *)&server, sizeof(struct sockaddr_in));
+server.sin_family = AF_INET;
+server.sin_port = htons(port);
+server.sin_addr.s_addr = htonl(INADDR_ANY);
+if (bind(sd, (struct sockaddr *)&server, sizeof(server)) == -1){
+fprintf(stderr, "Can't bind name to socket\n");
+exit(1);
+}
+/* queue up to 5 connect requests */
+listen(sd, 5);
+(void) signal(SIGCHLD, reaper);
+while(1) {
+client_len = sizeof(client);
+new_sd = accept(sd, (struct sockaddr *)&client, &client_len);
+if(new_sd < 0){
+fprintf(stderr, "Can't accept client \n");
+exit(1);
+}
+switch (fork()){
+case 0: /* child */
+(void) close(sd);
+exit(echod(new_sd));
+default: /* parent */
+(void) close(new_sd);
+break;
+case -1:
+fprintf(stderr, "fork: error\n");
+}
+}
+}
+/* echod program */
+int echod(int sd)
+{
+char *bp, buf[BUFLEN];
+int n, bytes_to_read;
+while(n = read(sd, buf, BUFLEN))
+write(sd, buf, n);
+close(sd);
+return(0);
+}
+/* reaper */
+void reaper(int sig)
+{
+int status;
+while(wait3(&status, WNOHANG, (struct rusage *)0) >= 0);
 }
